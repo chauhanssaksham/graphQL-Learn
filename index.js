7d4eaacf5162bba7a13f81288d025b19eb43f36d
Saksham <chauhanssaksham@gmail.com>
2020-04-14 00:35:59 +0530

    HelloWorld Query example

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
+const expressGraphQL = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
 const PORT = 5000;
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: ()=>({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+});
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}));
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
